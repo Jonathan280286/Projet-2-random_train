b729bbc01ab5afd40ecc76e16c180271c7d1c678
frederic andolina <fredericandolina@GearS3-82E9.localdomain>
2022-02-10 10:13:38 +0100

    create logo

--- src/Pages/accueil.js
+++ src/Pages/accueil.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../components/logo';
 
 const accueil = () => {
     return (
         <div>
-            
+            <Logo />
         </div>
     );
 };
--- src/components/logo.js
+++ src/components/logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <div>
-        
+        <div className='logo'>
+        <img   src='./Images/logoweekend.png' alt='image logo' /> 
         </div>
     );
 };
