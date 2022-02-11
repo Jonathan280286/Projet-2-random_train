409cb3be95fbe41776171cbb908d94b5ede31b01
JeanMichel999 <93377201+JeanMichel999@users.noreply.github.com>
2022-02-11 15:03:11 +0100

    version 3

--- src/components/horaireResult.js
+++ src/components/horaireResult.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const HoraireResult = () => {
+
+const [horaires, setGetHoraires]=useState("")
+
+useEffect(() =>{
+    axios
+      .get('https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:SNCF:87391003/departures?datetime=20220131T160312')
+      .then((response) => response.data)
+      .then((data) => {
+        setInformations(data.display_informations[0]);
+      }, []);
+
     return (
         <div>
-            Components - Réserver un train - Horaires result<br />
+          <div className='titre-horaireResult'>Components - Réserver un train - Horaires result</div>
+          <p>{horaires.labels}</p>
             <img   src='./Images/horaire.jpg' alt='horaire train' /> 
         </div>
     );
-};
 
 export default HoraireResult;
+
+
+
+
+
