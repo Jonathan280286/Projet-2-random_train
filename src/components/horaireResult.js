131a1c892fb1013ed78bbbd04e1a16f3ec46b7fa
JeanMichel999 <93377201+JeanMichel999@users.noreply.github.com>
2022-02-18 09:09:11 +0100

    version const horaireResult

--- src/components/horaireResult.js
+++ src/components/horaireResult.js
@@ -2,10 +2,9 @@ import './horaireResult.css'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-function HoraireResult() {
+function HoraireResult(props) {
 
-
-  const [horaires, setHoraires] = useState("")
+  const [horaires, setHoraires] = useState([])
 
   useEffect(() => {
     axios.get('https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:SNCF:87391003/departures?datetime=20220131T160312', {
@@ -18,19 +17,23 @@ function HoraireResult() {
         console.log(response.data.departures)
       })
   }, [])
+
+/* Fonction pour trier l'horaire de départ */
+  const transformHoraire = (heure) => {
+    return heure.slice(9).split('').splice(0, 4).concat(' min').toString().replace(/\B(?=(\d{5})+(?!\d))/g, "h");
+  }
   return (
     <div className='departures'>
-
       <span className="titre_depart">Prochains départs de trains</span>
       {horaires.length > 0 &&
         <ul>
-          {horaires.filter((transilien) => transilien.display_informations.code !== "N").filter((filtre, index) => index === 0 || index === 1).map(departure => {
-            return <li key={departure.display_informations.trip_short_name}>
+          {horaires.filter((transilien) => transilien.display_informations.code !== "N").filter((filtre, index) => index === 0 || index === 1).map((departure) => {
+            return        <li key={departure.display_informations.trip_short_name}>
               <h4 className="horaireResult_depart">{departure.stop_point.name}</h4>
               <h4 className='input'>Gare d'arrivée : {departure.display_informations.direction}</h4>
               <h4 className='input'>Train : {departure.display_informations.network}</h4>
               <h4 className='input'>Numéro du train : {departure.display_informations.trip_short_name}</h4>
-              <h4 className='input'>Heure de départ : {departure.stop_date_time.departure_date_time.slice(9).split('').splice(0, 4).concat(' min')}</h4>
+              <h4 className='input'>Heure de départ : {transformHoraire(departure.stop_date_time.departure_date_time)}</h4>
             </li>
           })}
         </ul>
@@ -41,8 +44,3 @@ function HoraireResult() {
 }
 
 export default HoraireResult;
-
-
-
-
-
